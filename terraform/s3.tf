a6134de23aecc8e282ad159c3eea0991270855fc
pmills-se <108750715+pmills-se@users.noreply.github.com>
2022-11-01 10:19:50 -0400

    final main commit

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,7 +4,23 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket
+  bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "sportsengine-dev-terraform-state"
+    key            = "terraform-demo/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    profile        = "se-dev"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "se-dev"
 }
