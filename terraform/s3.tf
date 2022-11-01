794cde90fa755142f2ba58067e99e48324b10f0b
pmills-se <108750715+pmills-se@users.noreply.github.com>
2022-11-01 10:46:17 -0400

    add website config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,10 +5,35 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
+  acl    = "public-read"
 
 }
 
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "index.html"
+  source = "index.html"
+
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket_website" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
+
+resource "aws_s3_bucket_cors_configuration" "bucket_cors" {
+  bucket = aws_s3_bucket.bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+
+}
 terraform {
   backend "s3" {
     bucket         = "sportsengine-dev-terraform-state"
