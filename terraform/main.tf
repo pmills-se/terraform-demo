ce64551563151a27c6fc872cb8f0a372cf522359
pmills-se <108750715+pmills-se@users.noreply.github.com>
2022-11-01 12:17:53 -0400

    remove s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "sportsengine-dev-terraform-state"
-    key            = "terraform-demo/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    profile        = "se-dev"
-    dynamodb_table = "terraform-lock"
-  }
-}
-
 provider "aws" {
   region  = "us-east-1"
   profile = "se-dev"
